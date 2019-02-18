899d317714bbc87b6c4aff0a2e562be5de5e1db5
YoumingWu07 <www.199597@qq.com>
2019-02-18 17:21:26 +0800

    Django第一个项目

---
+++ Django/project1/cmdb/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+from cmdb import models
+
+# Create your views here.
+
+user_list = [
+	{"user":"jack", "pwd":"abc"},
+	{"user":"tom", "pwd":"ABC"},
+]
+
+def index(request):
+	#request.POST
+	#request.GET
+	#return HttpResponse("hello world!")
+	if request.method == "POST":
+		username = request.POST.get("username",None)
+		password = request.POST.get("password",None)
+		models.UserInfo.objects.create(user=username, pwd=password)
+	user_list = models.UserInfo.objects.all()
+	return render(request, "index.html",{"data":user_list})
